231ffddab794bfa68d0c1f3bf64279ac4dea532e
job <superjob@totiming.com>
2016-07-26 14:35:44 +0800

    rename

--- js/itman70s.menu.js
+++ js/itman70s.menu.js
@@ -1,22 +1,22 @@
 
-var itman70s_menu = '<li><a href="http://itman70s.github.io/" data-placement="left" title="To ItMan70s.github.io">ITMan70s</a></li>\n' + 
-            '<li><a href="https://github.com/ItMan70s/tangrammy" data-placement="left" title="To Tangrammy page">Tangrammy</a></li>\n' + 
-            '<li><a href="http://itman70s.github.io/richtext" data-placement="left" title="To github.io/richtext">Rich Text</a></li>\n' + 
-            '<li><a href="http://itman70s.github.io/angular-movable" data-placement="left" title="To github.io/angular-movable">AngularJS Movable</a></li>\n' + 
-            '<li><a href="http://itman70s.github.io/transform.html" data-placement="left" title="transform for timestamp base64 urlencode">Transform</a></li>\n' + 
-            '<li><a href="http://itman70s.github.io/about.html" data-placement="left" title="To know itman70s?">About</a></li>\n' + 
+var ItManTos_menu = '<li><a href="http://ItManTos.github.io/" data-placement="left" title="To ItManTos.github.io">ItManTos</a></li>\n' + 
+            '<li><a href="https://github.com/ItManTos/tangrammy" data-placement="left" title="To Tangrammy page">Tangrammy</a></li>\n' + 
+            '<li><a href="http://ItManTos.github.io/richtext" data-placement="left" title="To github.io/richtext">Rich Text</a></li>\n' + 
+            '<li><a href="http://ItManTos.github.io/angular-movable" data-placement="left" title="To github.io/angular-movable">AngularJS Movable</a></li>\n' + 
+            '<li><a href="http://ItManTos.github.io/transform.html" data-placement="left" title="transform for timestamp base64 urlencode">Transform</a></li>\n' + 
+            '<li><a href="http://ItManTos.github.io/about.html" data-placement="left" title="To know ItManTos?">About</a></li>\n' + 
             '<li class="print"><a href="javascript:;" data-placement="left" title="Print current page">Print</a></li>\n';
 		
 $(document).ready(function(){
 try {
-	var head = $(".itman70s_menu");
-	head.attr("title", "All about ItMan70s.github.io");
+	var head = $(".ItManTos_menu");
+	head.attr("title", "All about ItManTos.github.io");
 	head.find("img").attr("src", "/img/logo_w.png");
 	
-	$(".itman70s_sub_menu").html(itman70s_menu);
+	$(".ItManTos_sub_menu").html(ItManTos_menu);
 	$('[data-placement]').tooltip();
 	
-	$(".itman70s_sub_menu").parents(".pull-right").append('<a href="https://github.com/ItMan70s"><img style="position: absolute; right: 0; border: 0;" src="/img/forkme.png" title="Fork me on GitHub"></a>');
+	$(".ItManTos_sub_menu").parents(".pull-right").append('<a href="https://github.com/ItManTos"><img style="position: absolute; right: 0; border: 0;" src="/img/forkme.png" title="Fork me on GitHub"></a>');
 		
 	$('.print').on('click', function(){	
 		$('.print').hide();
